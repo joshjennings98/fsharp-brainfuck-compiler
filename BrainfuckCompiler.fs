156d6e770fdf6c950374accbcb3480be43d0210d
Josh <jenninjl01@gmail.com>
2019-04-09 21:11:23 +0100

    Reworked tokeniser and added optimisations

--- BrainfuckCompiler.fs
+++ BrainfuckCompiler.fs
@@ -11,6 +11,7 @@ To Do:
 - Add compile arguments for brainfuck such as cell size, wrapping options, etc.
 - Turn into something actually usable without just modifying the test variable at the bottom of the file
 - Do a write up in the readme
+- Maybe convert tuples to 'of int'
 *)
 
 type SyntaxError =
@@ -25,15 +26,17 @@ type Instruction =
   | DecrementCell
   | WriteChar
   | GetChar
+  | Add of int * int //Add second int to (pointer location + first int)
+  | Sub of int * int //Subtract second int to (pointer location + first int)
+  | Set of int //Set cell at pointer to first int
 
 type Token = Instruction * int
 
-let filterByList (list : 'a list) (input : 'a list) : 'a list = 
-    List.filter (fun el1 -> (List.exists (fun el2 -> el2 = el1) list)) input
-
-let tokenise (input : string) : Result<Token list, SyntaxError> =
+let tokenise (input : string) : Instruction list =
   let mutable openBrackets = 0
   let validChars = ['['; ']';'>'; '<'; '+'; '-'; '.'; ',']
+  let filterByList (list : 'a list) (input : 'a list) : 'a list = 
+    List.filter (fun el1 -> (List.exists (fun el2 -> el2 = el1) list)) input
   let toInstruction (x : char) : Instruction =
     match x with
     | '[' -> LBracket
@@ -45,15 +48,94 @@ let tokenise (input : string) : Result<Token list, SyntaxError> =
     | '.' -> WriteChar
     | ',' -> GetChar
     | _ -> failwithf "Won't happen since all other characters are filtered out prior to this being called."
-  let inputList = 
-    input 
-    |> Seq.toList 
-    |> filterByList validChars 
-    |> List.map toInstruction
-  let concat (x : Instruction) = function
+  input 
+  |> Seq.toList 
+  |> filterByList validChars 
+  |> List.map toInstruction
+
+let optimise (input : Instruction list) : Result<Token list, SyntaxError> =
+  let concatIncrDecr (x : Instruction) = function
     | (i, count) :: t when i = x && i <> LBracket && i <> RBracket -> (i, count + 1) :: t 
     | t -> (x, 1) :: t
-  let tokenList = List.foldBack concat inputList []
+  let mergeSuccessiveIncrDecr (list : Token list) = 
+    let rec loop acc list =
+      match list with
+      | x1::x2::xs ->
+        match (x1 |> fst), (x2 |> fst) with
+        | IncrementCell, DecrementCell -> loop acc ((IncrementCell, (x1 |> snd) - (x2 |> snd))::xs)
+        | DecrementCell, IncrementCell -> loop acc ((IncrementCell, (x2 |> snd) - (x1 |> snd))::xs)
+        | IncrementPointer, DecrementPointer -> loop acc ((IncrementPointer, (x1 |> snd) - (x2 |> snd))::xs)
+        | DecrementPointer, IncrementPointer -> loop acc ((IncrementPointer, (x2 |> snd) - (x1 |> snd))::xs)
+        | _ -> loop (x1::acc) (x2::xs)
+      | x::xs -> loop (x::acc) xs
+      | _ -> acc |> List.rev
+    loop [] list
+  let simplifyAddSub (list : Token list) = 
+    let rec loop acc list =
+      match list with 
+      | x1::x2::x3::xs -> 
+        match (x1 |> fst), (x2 |> fst), (x3 |> fst) with
+        | IncrementPointer, IncrementCell, DecrementPointer ->
+          let addition = Add ((x1 |> snd),(x2 |> snd))
+          loop acc ((addition, 1)::(IncrementPointer, (x1 |> snd) - (x3 |> snd))::xs)  
+        | DecrementPointer, IncrementCell, IncrementPointer ->
+          let addition = Add (-1 * (x1 |> snd),(x2 |> snd))
+          loop acc ((addition, 1)::(IncrementPointer, (x3 |> snd) - (x1 |> snd))::xs)
+        | IncrementPointer, IncrementCell, IncrementPointer ->
+          let addition = Add ((x1 |> snd),(x2 |> snd))
+          loop acc ((addition, 1)::(IncrementPointer, (x3 |> snd) + (x1 |> snd))::xs)
+        | DecrementPointer, IncrementCell, DecrementPointer ->
+          let addition = Add (-1 * (x1 |> snd),(x2 |> snd))
+          loop acc ((addition, 1)::(DecrementPointer, (x3 |> snd) + (x1 |> snd))::xs)
+        | LBracket, IncrementCell, RBracket ->
+          loop acc ((Set 0, 1)::xs)
+        | IncrementPointer, DecrementCell, DecrementPointer ->
+          let subtraction = Sub ((x1 |> snd),(x2 |> snd))
+          loop acc ((subtraction, 1)::(IncrementPointer, (x1 |> snd) - (x3 |> snd))::xs)  
+        | DecrementPointer, DecrementCell, IncrementPointer ->
+          let subtraction = Add (-1 * (x1 |> snd),(x2 |> snd))
+          loop acc ((subtraction, 1)::(IncrementPointer, (x3 |> snd) - (x1 |> snd))::xs)
+        | IncrementPointer, DecrementCell, IncrementPointer ->
+          let subtraction = Add ((x1 |> snd),(x2 |> snd))
+          loop acc ((subtraction, 1)::(IncrementPointer, (x3 |> snd) + (x1 |> snd))::xs)
+        | DecrementPointer, DecrementCell, DecrementPointer ->
+          let subtraction = Add (-1 * (x1 |> snd),(x2 |> snd))
+          loop acc ((subtraction, 1)::(DecrementPointer, (x3 |> snd) + (x1 |> snd))::xs)
+        | LBracket, DecrementCell, RBracket ->
+          loop acc ((Set 0, 1)::xs)        
+        | _ -> loop (x1::acc) (x2::x3::xs)
+      | x::xs -> loop (x::acc) xs
+      | _ -> acc |> List.rev
+    loop [] list
+  let removeBeforeSet (list : Token list) = 
+    let rec loop acc list =
+      match list with
+      | x1::x2::xs ->
+        match (x1 |> fst), (x2 |> fst) with
+        | IncrementCell, Set i -> loop acc ((Set i, (x2 |> snd))::xs)
+        | DecrementCell, Set i -> loop acc ((Set i, (x2 |> snd))::xs)
+        | _ -> loop (x1::acc) (x2::xs)
+      | x::xs -> loop (x::acc) xs
+      | _ -> acc |> List.rev
+    loop [] list
+  
+  let fixIncrDecr (x : Token) = //This function isn't really neccessary but it makes it look nicer when its tokenised
+    match x with
+    | (IncrementCell, i) when i < 0 -> (DecrementCell, -1 * i)
+    | (DecrementCell, i) when i < 0 -> (IncrementCell, -1 * i)
+    | (IncrementPointer, i) when i < 0 -> (DecrementPointer, -1 * i)
+    | (DecrementPointer, i) when i < 0 -> (IncrementPointer, -1 * i)
+    | (Add(x, y), z) when y < 0 -> (Sub(x, y * -1), z)
+    | (Sub(x, y), z) when y < 0 -> (Add(x, y * -1), z)
+    | _ -> x
+  let tokenList = 
+    []
+    |> List.foldBack concatIncrDecr input
+    |> mergeSuccessiveIncrDecr
+    |> simplifyAddSub
+    |> removeBeforeSet
+    |> List.map fixIncrDecr
+    |> List.filter (fun el -> (el |> snd) <> 0)
   if (List.fold (fun acc el -> 
     match (el |> fst) with 
     | LBracket -> acc + 1 
@@ -62,6 +144,9 @@ let tokenise (input : string) : Result<Token list, SyntaxError> =
   ) 0 tokenList) = 0
   then tokenList |> Ok
   else ``Unclosed brackets`` |> Error
+
+
+
 
 let compile (input : Result<Token list, SyntaxError>) : Result<string, SyntaxError> =
   let mutable pointer = 0
@@ -98,11 +183,10 @@ let compile (input : Result<Token list, SyntaxError>) : Result<string, SyntaxErr
   | Error e -> e |> Error
 
 let test = 
-  "[>>>>[<<[>>>>]>>>+++<<---<<<]-]>-"
-  |> tokenise 
-  |> compile
+  ">>><<<<<<++---[---]-->"
+  |> tokenise
+  |> optimise 
+  //|> compile
 
-match test with
-| Ok x -> printf "%A \n" x
-| Error e -> printf "Error: %A \n" e
+printf "%A \n" test
   
